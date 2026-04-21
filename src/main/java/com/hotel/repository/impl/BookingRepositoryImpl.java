2f3929fd88a6dacb531870b5bb02d8b82f242376
icondenimStore <tn696199@gmail.com>
2026-04-21 16:22:51 +0700

    fix: Relationship about Booking & Room

---
+++ src/main/java/com/hotel/repository/impl/BookingRepositoryImpl.java
@@ -0,0 +1,8 @@
+package com.hotel.repository.impl;
+
+import com.hotel.repository.BookingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookingRepositoryImpl implements BookingRepository {
+}
