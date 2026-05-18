93a74203534147894617954babbbddb341ce7e7b
icondenimStore <tn696199@gmail.com>
2026-05-18 18:36:08 +0700

    feat: Customer View

--- springserver/src/main/java/com/hotel/controllers/admin/CustomerController.java
+++ springserver/src/main/java/com/hotel/controllers/admin/CustomerController.java
@@ -1,10 +1,14 @@
 package com.hotel.controllers.admin;
 
+import com.hotel.entity.Customer;
+import com.hotel.entity.Service;
 import com.hotel.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,7 +19,16 @@ public class CustomerController {
 
     @GetMapping("/customers")
     public String customerView(Model model) {
+        model.addAttribute("customer", new Customer());
         model.addAttribute("listCustomers", customerService.getAllCustomers());
+        return "customer";
+    }
+
+    @PostMapping("/customers")
+    public String processCustomer(@ModelAttribute(name = "customer") Customer customer) {
+        System.out.println(customer.getId());
+        System.out.println(customer.getName());
+        System.out.println(customer.getActive());
         return "customer";
     }
 }
