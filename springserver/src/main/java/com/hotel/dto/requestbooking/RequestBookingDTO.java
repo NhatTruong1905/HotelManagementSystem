cc5ee153cc55b07a9ec162cbdd8195b4aaa3dc63
icondenimStore <tn696199@gmail.com>
2026-06-03 03:31:08 +0700

    fix: format

--- springserver/src/main/java/com/hotel/dto/requestbooking/RequestBookingDTO.java
+++ springserver/src/main/java/com/hotel/dto/requestbooking/RequestBookingDTO.java
@@ -12,8 +12,8 @@ public class RequestBookingDTO {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate expectedCheckIn;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private Integer id;
     private LocalDate expectedCheckOut;
+    private Integer id;
     private List<DetailRoomDTO> rooms;
     private List<DetailServiceDTO> services;
     private DetailCustomerDTO customer;
