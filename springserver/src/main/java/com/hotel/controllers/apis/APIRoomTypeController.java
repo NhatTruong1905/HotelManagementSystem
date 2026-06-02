f88f24b2643c8eae96bb46bcb701abe3620179fb
icondenimStore <tn696199@gmail.com>
2026-06-02 07:06:04 +0700

    feat: Add @CrossOrigin

--- springserver/src/main/java/com/hotel/controllers/apis/APIRoomTypeController.java
+++ springserver/src/main/java/com/hotel/controllers/apis/APIRoomTypeController.java
@@ -17,6 +17,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 @PropertySource("classpath:configs.properties")
+@CrossOrigin
 public class APIRoomTypeController {
     @Autowired
     private Environment env;
